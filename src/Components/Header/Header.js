1b6a89cbebc328e7c871c95fbef0c2f874794462
moody blues <toorm@mail.gvsu.edu>
2019-03-30 23:17:16 -0400

    fixed header added, first section added with text which overflows the page to test the fixed header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+	
+let Header = props => {
+	return (
+		<div style={{
+				borderBottom:'3px solid #F3F7F8',
+				backgroundColor:'#5F4A62',
+				color:'white'}}
+				className='container-fluid fixed-top'>
+			<div className='text-center p-3'>
+				<h1 className='font-weight-bold'> Password Anatomy </h1>
+				<div className='w-100' />
+				<div className='row justify-content-center'>
+					<div style={{
+						backgroundColor: 'white',
+						color: 'black', 
+						width: '300px',
+						height: '40px',
+						boxShadow: '0px 0px 1px 3px black'}} className='col-4 m-3 pl-2 text-left'>
+						<h5 style={{
+							lineHeight: '40px'
+							}} className='m-0'>Password</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
---
+++ src/Components/Intro/Intro.js
@@ -0,0 +1,56 @@
+import React from 'react';
+	
+let Intro = props => {
+	return (
+		<div style={{backgroundColor: '#70bb4f', marginTop: '175px'}} 
+			className='container p-0'>
+			<div className='text-left p-3'>
+				<h2 className='font-weight-bold'> Welcome! </h2>
+			</div>
+			<div className='w-100' />
+			<div style={{backgroundColor: '#589B3B', width: '100%'}} >
+				<ul>
+			        <li> hello world </li>
+			        <li> hello jupiter </li>
+			        <li> hello mars </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+			        <li> hello world </li>
+				    <li> hello jupiter </li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Intro;
