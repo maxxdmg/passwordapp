0eda99b11e8e93d0c66edc5404eac2024e5a828a
unknown <fayetteg@mail.gvsu.edu>
2019-04-08 17:40:51 -0400

    Added dictionary attacks

--- src/Components/Step2/Step2.js
+++ src/Components/Step2/Step2.js
@@ -8,11 +8,18 @@ let Step2 = props => {
         <div style={{ backgroundColor: '#72AE5F', marginTop: '50px' }}
             className={cssClass}>
             <div className='text-left p-3'>
-                <h2 className='font-weight-bold'> Random words & dictionary attacks </h2>
+                <h2 className='font-weight-bold'> Common phrases, random words & dictionary attacks </h2>
             </div>
             <div className='w-100' />
             <div style={{ backgroundColor: '#72D75F', width: '100%', padding: '3px', paddingTop: '12px', height: '500px'}} >
-                <p>Text goes here!</p>
+                  <p style={{'textIndent': '40px'}} className='p-3'>A common method for creating long passwords is to use a common phrase or
+                  a string of related words, such as "ThisIsMyPassword" or "GreenTreeOutsideGround".  While long passwords like these are resitant
+                  to traditional brute force attacks, they are still vulnerable to a form of brute force attack known as a dictionary attack.   
+                  A dictionary attack is a form of password cracking that checks for commonly used passwords or strings of related words.  
+                  The attacker has a list of related words and common passwords (known as a dictionary) and checks combinations until the correct password is found.  
+                  Passwords such as “ThisIsMyPassword” are not recommended because they are commonly used and are likely to be included in an attacker's dictionary.
+                  Also, passwords such as "GreenTreeOutsideGround" are not recommended because they contain realted words which can be successfully guessed using a dictionary attack.
+                </p>
             </div>
         </div>
     );
