9647fffce553e90d6d3695e07fb5e3232444b134
moody blues <toorm@mail.gvsu.edu>
2019-04-13 14:16:32 -0400

    zIndex error fixed ?

--- src/Components/Step1/Step1.js
+++ src/Components/Step1/Step1.js
@@ -5,7 +5,7 @@ let Step1 = props => {
     if (props.transition)
         cssClass += ' ' + props.transition;
     return (
-        <div style={{ backgroundColor: '#EC9717FF', marginTop: '50px',zIndex: 5}}
+        <div style={{ backgroundColor: '#EC9717FF', marginTop: '50px',zIndex: '5'}}
             className={cssClass}>
             <div className='text-left p-3'>
                 <h2 className='font-weight-bold'> Longer Passwords & Brute Forcing  </h2>
@@ -18,14 +18,13 @@ let Step1 = props => {
                  brute forcing passwords, all tools and methods for cracking is built upon the idea of brute force.
                  How the length of a password deters cracking is very simple. The time taken to guess a password
                  gets exponentially larger the longer a password is. For a complex password the amount of time needed
-                 to guess a password with basic brute forcing is unreasonably long. For example the time it takes to 
+                 to guess a password with basic brute forcing is unreasonably long. For example, according to  the time it takes to 
                  crack (which is just simply guessing) the 7 character long password 'abcdefg' is 0.29 millseconds 
                  as of 2015. That is pretty quick, by changing the password from the seven character long 'abcdefg'
                  to the 12 character long 'abcdefghijkl' the time to crack is increased to 2 centuries. That is a 
                  giant jump for an added 5 extra characters which shows how impactful the length of a password 
                  truly is.
                 </p>
-                
             </div>
         </div>
     );
