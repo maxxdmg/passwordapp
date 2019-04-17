b5e30bbc57876f1c31dc9536f75967b480907560
moody blues <toorm@mail.gvsu.edu>
2019-04-17 11:43:42 -0400

    final commit

---
+++ precache-manifest.016ed370cbae3055e2a19a1b950d64ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8f4de47823a1c2fd2298392eb7f262e",
+    "url": "/passwordapp/static/media/git_white.d8f4de47.png"
+  },
+  {
+    "revision": "9ce68bcab87f651b5a6bd16489896ef7",
+    "url": "/passwordapp/static/media/git.9ce68bca.png"
+  },
+  {
+    "revision": "f1c958413cab32e555c9ec075c65248d",
+    "url": "/passwordapp/static/media/bg.f1c95841.PNG"
+  },
+  {
+    "revision": "d2e2bebc974b65502950",
+    "url": "/passwordapp/static/js/runtime~main.109d3304.js"
+  },
+  {
+    "revision": "f70adadbc7306b86c482",
+    "url": "/passwordapp/static/js/main.fa3c75ed.chunk.js"
+  },
+  {
+    "revision": "cf01766ae709ea151581",
+    "url": "/passwordapp/static/js/2.5d4abc1e.chunk.js"
+  },
+  {
+    "revision": "f70adadbc7306b86c482",
+    "url": "/passwordapp/static/css/main.95da7154.chunk.css"
+  },
+  {
+    "revision": "60f939d85e973652bb74aebae6a41272",
+    "url": "/passwordapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8a374cdede28b9213939a523cbfea83.js"
+  "/passwordapp/precache-manifest.016ed370cbae3055e2a19a1b950d64ff.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/passwordapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
